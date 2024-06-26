8f64970ca01ffab14b5e4c16c80ff51ab38318c0
Daniel Lopes <aeciodlins@outlook.com>
2024-06-25 23:11:13 -0300

    Add files via upload

---
+++ band_generator_name.py
@@ -0,0 +1,11 @@
+# Welcome
+print("Welcome to the Band Name Generator!")
+
+# The user city 
+my_city = input("What's the name of the city you grew up in?\n")
+
+# The user pet
+my_pet = input("What's your pet's name?\n")
+
+# The band name
+print("The band name could be " + my_city + " " + my_pet)
